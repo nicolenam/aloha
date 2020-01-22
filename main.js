7c7d693577d4f649de8f40b295c41a815cc6b9d0
nicole <eunjungnam@gmail.com>
2020-01-22 08:54:00 -0500

    add corousel

---
+++ main.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+
+
+
+
+
+
+
+
+  // var elem = document.querySelector('.main-carousel');
+    // var flkty = new Flickity( elem, {
+    //   // options
+    //   cellAlign: 'left',
+    //   contain: true
+    // });
+    
+    // // element argument can be a selector string
+    // //   for an individual element
+    // var flkty = new Flickity( '.main-carousel', {
+    //   // options
+    // });
+
+// const cartBtn = document.getElementById("knitDress")
+
+// let clicks = 0
+
+// cartBtn.addEventListener("click", function(e) {
+// clicks ++
+// console.log(clicks)
+
+
+
+// })
+
+});
