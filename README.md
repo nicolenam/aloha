07aebd8c739b3f1aa6f2c9af512eb89c29a479db
nicole <eunjungnam@gmail.com>
2020-01-25 21:58:17 -0500

    feat: add initial readme file

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Aloha - Project 1 
 
 This is my Aloha project...
+
+Objective: 
+The objective of this project was to create a website for Aloha Apparel Co. based on the mock-up images provided by RED Academy using HTML, CSS and Javascript.
+
+Technologies Used:
+
+HTML
+CSS
+Javascript
+
+Personal Learnings:
+Learned how to implement a mobile-first, responsive layout using flexbox. Used external sources such as Flickity and Font Awesome and linked them to index.html file so that they are accessible in Visual Studio Code. Learned how to use Visual Studio Code, and developer tools. Backed up files to Github using terminal commands. Started to use javascript language for the first time and added some functionality to the website. 
+
+Installation Instructions:
+This website does not have a domain, it is only a mock-up version of Aloha Apparel. Please download the files that are uploaded in Github in order to interact with the pages. 
+
+Written by: Nicole Nam
--- main.js
+++ main.js
@@ -1,25 +1,42 @@
 document.addEventListener("DOMContentLoaded", function() {
+  let elem = document.querySelector(".carousel");
 
-// FEATURED STARTS HERE   
-let elem = document.querySelector('.carousel');
-
-let flkty = new Flickity( elem, {
+  let flkty = new Flickity(elem, {
     wrapAround: true,
-    cellAlign: 'left',
+    cellAlign: "left",
     contain: true
   });
-// FEATURED ENDS HERE 
 
+  document.getElementById("subBtn").addEventListener("click", function() {
+    const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
+    // if(InputEventmatch(mailFormat))
+    alert("Thank you for Subscribing !");
+  });
 
-// SUBSCRIPTION STARTS HERE 
+  const btn = document.getElementById("cartBtn")
+  btn.addEventListener("click", function(){
+alert("buy more")
 
-document.getElementById('subBtn').addEventListener("click", function() {
-  alert("Thank you for Subscribing !");
-});
+  })
+  // function ValidateEmail(InputText){
 
+  //   let mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  //   if(InputText.value.match(mailFormat))
+  //   alert("Thank you for subscribing !");
+  //    return true;
+  // }else{
+  //   alert("You have entered an invalid email address!");
+  //   return false;
+  // }
 
+  
+//   const carBtn = document.querySelectorAll(".carBtn");
+//   cartBtn.forEach(function(btn){
+//     btn.addEventListener('click',function(e){
+// console.log(cartBtn)
+//     })
+//   })
 
-// SUBSCRIPTION ENDS HERE 
 
 });
